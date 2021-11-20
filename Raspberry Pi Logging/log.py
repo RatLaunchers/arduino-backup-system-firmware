2b12b735d6f03d75bd810bde4200bf1223e8e322
UserBlackBox <userblackbox@tutanota.com>
2021-11-19 21:42:17 -0500

    make it a big prettier

--- Raspberry Pi Logging/log.py
+++ Raspberry Pi Logging/log.py
@@ -13,6 +13,10 @@ agps_thread.run_thread()
 while True:
     log = open("sensorLog.csv", 'a', newline='')
     writer = csv.writer(log)
-    writer.writerow([str(datetime.now()), agps_thread.data_stream.lat, agps_thread.data_stream.lon])
+    writer.writerow([
+        str(datetime.now()),
+        agps_thread.data_stream.lat,
+        agps_thread.data_stream.lon
+        ])
     log.close()
     time.sleep(1)
